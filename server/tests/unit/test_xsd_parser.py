61d226f1d20d4804597912495938864de158e376
davelopez <46503462+davelopez@users.noreply.github.com>
2020-08-30 11:51:43 +0200

    Improve XSD parser tests

--- server/tests/unit/test_xsd_parser.py
+++ server/tests/unit/test_xsd_parser.py
@@ -1,5 +1,7 @@
+import pytest
+
 from lxml import etree
-from ...services.xsd.parser import XsdParser
+from ...services.xsd.parser import GalaxyToolXsdParser
 from ...services.xsd.constants import MSG_NO_DOCUMENTATION_AVAILABLE
 
 TEST_XSD = """<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
@@ -15,9 +17,9 @@ TEST_XSD = """<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
   </xs:element>
   <xs:complexType name="CustomComplexType">
     <xs:sequence>
-      <xs:element name="firstElement" type="xs:integer"/>
+      <xs:element name="firstElement" type="CustomIntSimpleType"/>
       <xs:element name="secondElement" type="xs:string"/>
-      <xs:element name="thirdElement" type="CustomIntSimpleType">
+      <xs:element name="thirdElement">
         <xs:complexType>
           <xs:sequence>
             <xs:element name="childElement" type="xs:integer">
@@ -34,9 +36,18 @@ TEST_XSD = """<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
           <xs:attribute name="testDate" type="xs:date"/>
         </xs:complexType>
       </xs:element>
+      <xs:element name="element_with_group">
+        <xs:complexType>
+          <xs:sequence>
+            <xs:group ref="TestGroup" minOccurs="0" maxOccurs="1"/>
+          </xs:sequence>
+          <xs:attribute name="testDate" type="xs:date"/>
+        </xs:complexType>
+      </xs:element>
     </xs:sequence>
     <xs:attribute name="id" type="xs:integer" use="required"/>
     <xs:attribute name="value" type="ValueSimpleType"/>
+    <xs:attributeGroup ref="TestAttrGroup"/>
   </xs:complexType>
   <xs:simpleType name="CustomIntSimpleType">
     <xs:restriction base="xs:integer">
@@ -51,84 +62,115 @@ TEST_XSD = """<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
         <xs:enumeration value="v3"/>
     </xs:restriction>
   </xs:simpleType>
+  <xs:attributeGroup name="TestAttrGroup">
+    <xs:attribute name="gattr1" type="xs:string"/>
+    <xs:attribute name="gattr2" type="xs:string"/>
+  </xs:attributeGroup>
+  <xs:group name="TestGroup">
+    <xs:sequence>
+      <xs:element name="group_elem1" type="xs:string"/>
+      <xs:element name="group_elem2" type="xs:string"/>
+    </xs:sequence>
+  </xs:group>
+</xs:schema>
+"""
+
+RECURSIVE_XSD = """<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+  <xs:element name="node" type="nodeType"/>
+  <xs:complexType name="nodeType">
+    <xs:sequence>
+      <xs:element name="node" type="nodeType"/>
+    </xs:sequence>
+  </xs:complexType>
 </xs:schema>
 """
 
 
-def _get_test_parser() -> XsdParser:
+@pytest.fixture(scope="module")
+def xsd_parser() -> GalaxyToolXsdParser:
     root = etree.fromstring(TEST_XSD)
-    parser = XsdParser(root)
+    parser = GalaxyToolXsdParser(root)
     return parser
 
 
-def test_get_tree_returns_valid_element_names():
-    parser = _get_test_parser()
-
-    tree = parser.get_tree()
-    root = tree.root
-
-    assert len(root.ancestors) == 0
-    assert len(root.descendants) == 4
-    assert root.name == "testElement"
-    assert root.children[0].name == "firstElement"
-    assert root.children[1].name == "secondElement"
-    assert root.children[2].name == "thirdElement"
-    assert root.children[2].children[0].name == "childElement"
+@pytest.fixture(scope="module")
+def recursive_xsd_parser() -> GalaxyToolXsdParser:
+    root = etree.fromstring(RECURSIVE_XSD)
+    parser = GalaxyToolXsdParser(root)
+    return parser
 
 
-def test_get_tree_returns_valid_attribute_names():
-    parser = _get_test_parser()
+class TestXsdParserClass:
+    def test_get_tree_returns_valid_element_names(self, xsd_parser):
+        tree = xsd_parser.get_tree()
+        root = tree.root
 
-    tree = parser.get_tree()
-    root = tree.root
+        assert len(root.ancestors) == 0
+        assert len(root.descendants) == 7
+        assert root.name == "testElement"
+        assert root.children[0].name == "firstElement"
+        assert root.children[1].name == "secondElement"
+        assert root.children[2].name == "thirdElement"
+        assert root.children[2].children[0].name == "childElement"
+        assert root.children[3].name == "element_with_group"
+        assert root.children[3].children[0].name == "group_elem1"
+        assert root.children[3].children[1].name == "group_elem2"
 
-    assert len(root.attributes) == 2
-    assert root.attributes["id"]
-    assert root.attributes["value"]
-    assert root.children[2].attributes["testDate"]
+    def test_get_tree_returns_valid_attribute_names_using_groups(
+        self, xsd_parser
+    ):
+        tree = xsd_parser.get_tree()
+        root = tree.root
 
+        assert len(root.attributes) == 4
+        assert root.attributes["id"]
+        assert root.attributes["value"]
+        assert root.attributes["gattr1"]
+        assert root.attributes["gattr2"]
 
-def test_tree_find_node_by_name_returns_expected_node():
-    parser = _get_test_parser()
-    tree = parser.get_tree()
-    expected = "childElement"
+    def test_get_tree_with_recursive_xsd_stops_recursion(
+        self, recursive_xsd_parser
+    ):
+        tree = recursive_xsd_parser.get_tree()
 
-    node = tree.find_node_by_name(expected)
+        assert len(tree.root.descendants) > 0
 
-    assert node.name == expected
+    def test_tree_find_node_by_name_returns_expected_node(self, xsd_parser):
+        tree = xsd_parser.get_tree()
+        expected = "childElement"
 
+        node = tree.find_node_by_name(expected)
 
-def test_tree_find_node_by_name_returns_None_when_node_not_found():
-    parser = _get_test_parser()
-    tree = parser.get_tree()
+        assert node.name == expected
 
-    node = tree.find_node_by_name("unknown")
+    def test_tree_find_node_by_name_returns_None_when_node_not_found(
+        self, xsd_parser,
+    ):
+        tree = xsd_parser.get_tree()
 
-    assert node is None
+        node = tree.find_node_by_name("unknown")
 
+        assert node is None
 
-def test_get_documentation_returns_valid_when_exists():
-    parser = _get_test_parser()
-    tree = parser.get_tree()
+    def test_get_documentation_returns_valid_when_exists(self, xsd_parser):
+        tree = xsd_parser.get_tree()
 
-    doc = tree.root.get_doc()
+        doc = tree.root.get_doc()
 
-    assert doc.value == "Documentation ``example``."
+        assert doc.value == "Documentation ``example``."
 
+    def test_get_documentation_returns_valid_other_language(self, xsd_parser):
+        tree = xsd_parser.get_tree()
 
-def test_get_documentation_returns_valid_when_language_is_given():
-    parser = _get_test_parser()
-    tree = parser.get_tree()
+        doc = tree.root.get_doc("es")
 
-    doc = tree.root.get_doc("es")
+        assert doc.value == "``Ejemplo`` de documentación."
 
-    assert doc.value == "``Ejemplo`` de documentación."
+    def test_get_documentation_should_return_no_documentation_when_not_exists(
+        self, xsd_parser,
+    ):
+        tree = xsd_parser.get_tree()
 
+        doc = tree.root.get_doc("de")
 
-def test_get_documentation_should_return_no_documentation_when_not_exists():
-    parser = _get_test_parser()
-    tree = parser.get_tree()
-
-    doc = tree.root.get_doc("de")
-
-    assert doc.value == MSG_NO_DOCUMENTATION_AVAILABLE
+        assert doc.value == MSG_NO_DOCUMENTATION_AVAILABLE
