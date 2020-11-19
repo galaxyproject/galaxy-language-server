9711869c868dfdc7b52abeac2fd9f091b25b5c8a
davelopez <46503462+davelopez@users.noreply.github.com>
2020-11-19 18:42:56 +0100

    Add tests
    Test documentation retrieval from xsd definitions or types

--- server/galaxyls/tests/unit/test_xsd_service.py
+++ server/galaxyls/tests/unit/test_xsd_service.py
@@ -1,21 +1,54 @@
-from ...services.xsd.types import XsdNode, XsdTree
 from pytest_mock import MockerFixture
-from ...services.context import XmlContext
-from ...services.xml.nodes import XmlAttributeKey, XmlElement
+
 from ...services.xsd.constants import MSG_NO_DOCUMENTATION_AVAILABLE
 from ...services.xsd.service import GalaxyToolXsdService
 
 
 class TestGalaxyToolXsdServiceClass:
     def test_get_documentation_for_unknown_node_attribute_returns_no_documentation(self, mocker: MockerFixture) -> None:
-        fake_xsd_root = XsdNode("tool", mocker.Mock())
-        fake_xsd_tree = XsdTree(fake_xsd_root)
-        fake_element = XmlElement()
-        fake_element.name = "tool"
-        fake_node = XmlAttributeKey("uknownAttr", 0, 10, fake_element)
-        fake_context = XmlContext(fake_xsd_tree.root, fake_node)
         service = GalaxyToolXsdService("Test")
+        fake_context = mocker.Mock()
+        fake_context.is_tag = True
+        fake_context.is_attribute_key = False
+        fake_context.stack = ["unknown"]
 
         doc = service.get_documentation_for(fake_context)
 
         assert doc.value == MSG_NO_DOCUMENTATION_AVAILABLE
+
+    def test_get_documentation_for_annotated_element(self, mocker: MockerFixture) -> None:
+        service = GalaxyToolXsdService("Test")
+        fake_context = mocker.Mock()
+        fake_context.is_tag = True
+        fake_context.is_attribute_key = False
+        fake_context.stack = ["tool"]
+
+        doc = service.get_documentation_for(fake_context)
+
+        assert doc.value
+        assert doc.value != MSG_NO_DOCUMENTATION_AVAILABLE
+
+    def test_get_documentation_for_element_using_annotated_type(self, mocker: MockerFixture) -> None:
+        service = GalaxyToolXsdService("Test")
+        fake_context = mocker.Mock()
+        fake_context.is_tag = True
+        fake_context.is_attribute_key = False
+        fake_context.stack = ["tool", "macros"]
+
+        doc = service.get_documentation_for(fake_context)
+
+        assert doc.value
+        assert doc.value != MSG_NO_DOCUMENTATION_AVAILABLE
+
+    def test_get_documentation_for_annotated_attribute(self, mocker: MockerFixture) -> None:
+        service = GalaxyToolXsdService("Test")
+        fake_context = mocker.Mock()
+        fake_context.is_tag = False
+        fake_context.is_attribute_key = True
+        fake_context.token.name = "id"
+        fake_context.stack = ["tool"]
+
+        doc = service.get_documentation_for(fake_context)
+
+        assert doc.value
+        assert doc.value != MSG_NO_DOCUMENTATION_AVAILABLE
