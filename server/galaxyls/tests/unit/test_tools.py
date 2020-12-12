de2935443607619ed9596e62d8efa514eded0420
davelopez <46503462+davelopez@users.noreply.github.com>
2020-12-12 23:00:50 +0100

    Add test files for outputs

--- server/galaxyls/tests/unit/test_tools.py
+++ server/galaxyls/tests/unit/test_tools.py
@@ -107,6 +107,8 @@ class TestGalaxyToolXmlDocumentClass:
             ("simple_params_01.xml", "simple_params_01_test.xml"),
             ("simple_repeat_01.xml", "simple_repeat_01_test.xml"),
             ("simple_section_01.xml", "simple_section_01_test.xml"),
+            ("simple_output_01.xml", "simple_output_01_test.xml"),
+            ("simple_output_02.xml", "simple_output_02_test.xml"),
         ],
     )
     def test_generate_test_suite_snippet_returns_expected_result(self, tool_file: str, expected_snippet_file: str) -> None:
@@ -116,7 +118,5 @@ class TestGalaxyToolXmlDocumentClass:
         generator = GalaxyToolTestSnippetGenerator(tool)
 
         actual_snippet = generator.generate_test_suite_snippet()
-
-        print(actual_snippet)
 
         assert actual_snippet == expected_snippet
