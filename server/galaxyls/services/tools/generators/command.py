4017ff6b359b5fa820dce28fbf6ffda6510188f6
davelopez <46503462+davelopez@users.noreply.github.com>
2020-12-21 21:19:03 +0100

    Generate basic params

--- server/galaxyls/services/tools/generators/command.py
+++ server/galaxyls/services/tools/generators/command.py
@@ -1,11 +1,31 @@
-from typing import List, Optional
+from typing import List, Optional, cast
 
-from galaxyls.services.tools.constants import COMMAND, CONFIGFILES, ENV_VARIABLES, INPUTS
+from anytree import PreOrderIter
+from galaxyls.services.tools.constants import (
+    ARGUMENT,
+    BOOLEAN,
+    COMMAND,
+    CONFIGFILES,
+    DASH,
+    DATA,
+    ENV_VARIABLES,
+    FLOAT,
+    INPUTS,
+    INTEGER,
+    NAME,
+    OPTIONAL,
+    TEXT,
+    TRUEVALUE,
+    TYPE,
+    UNDERSCORE,
+)
 from galaxyls.services.tools.document import GalaxyToolXmlDocument
 from galaxyls.services.tools.generators.snippets import SnippetGenerator
-from galaxyls.services.tools.inputs import GalaxyToolInputTree
+from galaxyls.services.tools.inputs import ConditionalInputNode, GalaxyToolInputTree, InputNode
 from galaxyls.services.xml.nodes import XmlElement
 from pygls.types import Position
+
+ARG_PLACEHOLDER = "TODO_argument"
 
 
 class GalaxyToolCommandSnippetGenerator(SnippetGenerator):
@@ -13,14 +33,13 @@ class GalaxyToolCommandSnippetGenerator(SnippetGenerator):
     section using the information already defined in the inputs and outputs of the tool XML wrapper.
     """
 
-    def __init__(self, tool_document: GalaxyToolXmlDocument) -> None:
-        super().__init__(tool_document)
+    def __init__(self, tool_document: GalaxyToolXmlDocument, tabSize: int = 4) -> None:
+        super().__init__(tool_document, tabSize)
 
-    def _build_snippet(self, tabSize: int = 4) -> Optional[str]:
-        spaces = " " * tabSize
+    def _build_snippet(self) -> Optional[str]:
         input_tree = self.tool_document.analyze_inputs()
         outputs = self.tool_document.get_outputs()
-        result_snippet = self._generate_command_snippet(input_tree, outputs, spaces)
+        result_snippet = self._generate_command_snippet(input_tree, outputs)
         create_section = not self.tool_document.has_section_content(COMMAND)
         if create_section:
             return f"<{COMMAND}><![CDATA[\n\n{result_snippet}\n\n]]>\n</{COMMAND}>\n"
@@ -57,5 +76,40 @@ class GalaxyToolCommandSnippetGenerator(SnippetGenerator):
                 return tool.get_position_before(section)
             return Position()
 
-    def _generate_command_snippet(self, input_tree: GalaxyToolInputTree, outputs: List[XmlElement], spaces: str = "  ") -> str:
-        return "## TODO Insert some cheetah code"
+    def _generate_command_snippet(self, input_tree: GalaxyToolInputTree, outputs: List[XmlElement]) -> str:
+        snippets = [
+            "## Auto-generated command section",
+            "## TODO: please review and edit this section as needed",
+            "\n## Inputs",
+        ]
+        for node in PreOrderIter(input_tree._root):
+            node = cast(InputNode, node)
+            if type(node) is ConditionalInputNode:
+                pass
+            else:
+                for param in node.params:
+                    snippets.append(self._input_to_cheetah(param))
+        snippets.append("\n## Outputs\n")
+        return "\n".join(snippets)
+
+    def _input_to_cheetah(self, input: XmlElement) -> str:
+        argument_attr = input.get_attribute(ARGUMENT)
+        name_attr = input.get_attribute(NAME)
+        if not name_attr and argument_attr:
+            name_attr = argument_attr.lstrip(DASH).replace(DASH, UNDERSCORE)
+        type_attr = input.get_attribute(TYPE)
+        if type_attr == BOOLEAN:
+            return f"\\${input.get_attribute(TRUEVALUE) or name_attr}"
+        if type_attr in [INTEGER, FLOAT]:
+            if input.get_attribute(OPTIONAL) == "true":
+                return (
+                    f"#if str(\\${name_attr}):\n"
+                    f"{self.indent_spaces}{self._get_argument_safe(argument_attr)} \\${name_attr}"
+                    f"#end if"
+                )
+        if type_attr in [TEXT, DATA]:
+            return f"{self._get_argument_safe(argument_attr)} '\\${name_attr}'"
+        return f"{self._get_argument_safe(argument_attr)} \\${name_attr}"
+
+    def _get_argument_safe(self, argument: Optional[str]) -> str:
+        return argument or self._get_next_tabstop_with_placeholder(ARG_PLACEHOLDER)
