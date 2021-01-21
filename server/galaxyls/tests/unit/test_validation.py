32ad7095b24e378d91f7f9c480b09e0e844bf780
davelopez <46503462+davelopez@users.noreply.github.com>
2021-01-21 17:00:15 +0100

    Add tests for document root detection

--- server/galaxyls/tests/unit/test_validation.py
+++ server/galaxyls/tests/unit/test_validation.py
@@ -1,6 +1,8 @@
 import pytest
 from lxml import etree
 
+from galaxyls.services.validation import DocumentValidator
+
 from ...services.xsd.constants import TOOL_XSD_FILE
 from ...services.xsd.validation import GalaxyToolValidationService
 from .sample_data import (
@@ -62,3 +64,34 @@ class TestGalaxyToolValidationServiceClass:
         actual = service.validate_document(xml_document)
 
         assert len(actual) == 1
+
+
+class TestDocumentValidatorClass:
+    @pytest.mark.parametrize(
+        "source, expected",
+        [
+            ("<tool>", True),
+            ("  <tool>", True),
+            ("\n<tool>", True),
+            ("\n  <tool>", True),
+            ("  \n  <tool>", True),
+            ("unexpected  <tool>", True),
+            ("  unexpected  <tool>", True),
+            ("\nunexpected\n  <tool>", True),
+            ('<?xml version="1.0" encoding="UTF-8"?><tool>', True),
+            ('<?xml version="1.0" encoding="UTF-8"?>\n<tool>', True),
+            ("<macros>", True),
+            ('<?xml version="1.0" encoding="UTF-8"?>\n<macros>', True),
+            ("", False),
+            ("   ", False),
+            ("test", False),
+            ("<test>", False),
+        ],
+    )
+    def test_has_valid_root_returns_expected(self, source: str, expected: bool) -> None:
+        document = TestUtils.to_document(source)
+        validator = DocumentValidator()
+
+        actual = validator.has_valid_root(document)
+
+        assert actual == expected
