6337038156aca45c006f1dd2c7b1ceb01ced8a96
davelopez <46503462+davelopez@users.noreply.github.com>
2021-09-12 20:06:38 +0200

    Update client to v0.6.0

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -3,7 +3,7 @@ export namespace Constants {
     export const LS_VENV_NAME = "glsenv";
     export const GALAXY_LS_PACKAGE = "galaxy-language-server";
     export const GALAXY_LS = "galaxyls";
-    export const GALAXY_LS_VERSION = "0.5.3";
+    export const GALAXY_LS_VERSION = "0.6.0";
     export const LANGUAGE_ID = "galaxytool";
     export const TOOL_DOCUMENT_EXTENSION = "xml";
 
