a7186a49ddff34523f95cbe2f5c0952ff47af8e2
davelopez <46503462+davelopez@users.noreply.github.com>
2021-10-17 20:27:27 +0200

    Adapt language server custom commands
    The custom commands were wrongly declared as features and this was working in the previous pygls version. The new version unveiled the bug, since custom commands have to be registered and their parameters parsed in a slighly different way.

--- server/galaxyls/constants.py
+++ server/galaxyls/constants.py
@@ -4,13 +4,13 @@ SERVER_NAME = "Galaxy Tools LS"
 
 
 class Commands:
-    AUTO_CLOSE_TAGS = "galaxytools.completion.autoCloseTags"
-    GENERATE_TESTS = "galaxytools.generate.tests"
-    GENERATE_COMMAND = "galaxytools.generate.command"
-    SORT_SINGLE_PARAM_ATTRS = "galaxytools.sort.singleParamAttributes"
-    SORT_DOCUMENT_PARAMS_ATTRS = "galaxytools.sort.documentParamsAttributes"
-    DISCOVER_TESTS = "galaxytools.tests.discover"
-    GENERATE_EXPANDED_DOCUMENT = "galaxytools.generate.expandedDocument"
+    AUTO_CLOSE_TAGS = "gls.completion.autoCloseTags"
+    GENERATE_TESTS = "gls.generate.tests"
+    GENERATE_COMMAND = "gls.generate.command"
+    SORT_SINGLE_PARAM_ATTRS = "gls.sort.singleParamAttributes"
+    SORT_DOCUMENT_PARAMS_ATTRS = "gls.sort.documentParamsAttributes"
+    DISCOVER_TESTS = "gls.tests.discover"
+    GENERATE_EXPANDED_DOCUMENT = "gls.generate.expandedDocument"
 
 
 class DiagnosticCodes:
