b5ad0ec864e6b16fb1e6cd5ab5e891760f2b9c1c
davelopez <46503462+davelopez@users.noreply.github.com>
2023-02-19 15:08:34 +0100

    Fix config value testing

--- server/galaxyls/tests/unit/test_config.py
+++ server/galaxyls/tests/unit/test_config.py
@@ -2,14 +2,20 @@ from galaxyls.config import (
     CompletionConfig,
     CompletionMode,
     GalaxyToolsConfiguration,
+    ServerConfig,
 )
 
 
 class TestGalaxyToolsConfigurationClass:
     def test_init_sets_properties(self) -> None:
-        fake_completion_config = CompletionConfig(mode=CompletionMode.INVOKE, auto_close_tags=False)
+        fake_server_config = ServerConfig()
+        fake_server_config.silent_install = True
+        fake_completion_config = CompletionConfig()
+        fake_completion_config.mode = CompletionMode.INVOKE
+        fake_completion_config.auto_close_tags = False
 
-        config = GalaxyToolsConfiguration(fake_completion_config)
+        config = GalaxyToolsConfiguration(server=fake_server_config, completion=fake_completion_config)
 
+        assert config.server.silent_install is True
         assert config.completion.mode == CompletionMode.INVOKE
-        assert not config.completion.auto_close_tags
+        assert config.completion.auto_close_tags is False
