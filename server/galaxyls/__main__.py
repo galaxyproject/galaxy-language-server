0bc1b0c66337574d5e5e93cc4efd5c8a138ee7c8
davelopez <46503462+davelopez@users.noreply.github.com>
2023-02-25 12:15:58 +0100

    Add websocket connection support

--- server/galaxyls/__main__.py
+++ server/galaxyls/__main__.py
@@ -9,18 +9,21 @@ logging.basicConfig(filename="galaxy-language-server.log", level=logging.DEBUG, 
 def add_arguments(parser: argparse.ArgumentParser) -> None:
     parser.description = "Galaxy Language Server"
 
-    parser.add_argument("--tcp", action="store_true", help="Use TCP server instead of stdio")
+    parser.add_argument("--tcp", action="store_true", help="Use TCP server")
+    parser.add_argument("--ws", action="store_true", help="Use WebSocket server")
     parser.add_argument("--host", default="127.0.0.1", help="Bind to this address")
     parser.add_argument("--port", type=int, default=2087, help="Bind to this port")
 
 
-def main() -> None:
+def main():
     parser = argparse.ArgumentParser()
     add_arguments(parser)
     args = parser.parse_args()
 
     if args.tcp:
         language_server.start_tcp(args.host, args.port)
+    elif args.ws:
+        language_server.start_ws(args.host, args.port)
     else:
         language_server.start_io()
 
